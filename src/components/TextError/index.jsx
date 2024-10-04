e529ce32e8dd3622425f9b8f12ede64ba1c91560
rafabrodrigues <166878952+rafabrodrigues@users.noreply.github.com>
2024-10-04 15:36:32 -0300

    Validando Login

---
+++ src/components/TextError/index.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const TextError = styled.Text`
+  color: red;
+  margin-bottom: 8px;
+  font-family: "Poppins_400Regular";
+  font-size: 12px;
+`;
--- src/screens/SignInScreen/index.jsx
+++ src/screens/SignInScreen/index.jsx
@@ -15,21 +15,35 @@ import InputSmooth from "../../components/InputSmooth";
 import InputPasswordSmooth from "../../components/InputPasswordSmooth";
 import { Button, ButtonText, ButtonWithIcon } from "../../components/Button";
 import { Link, LinkForgetPassword, LinkText } from "../../components/Link";
-import { supabase } from "../../Supabase/supabaseClient";
-import { useNavigation } from "@react-navigation/native";
 import { SubTitle } from "../../components/SubTitle";
 import { SpaceBetween } from "../../components/Containers/SpaceBetween";
+import { Controller, useForm } from "react-hook-form";
 
+import { supabase } from "../../Supabase/supabaseClient";
+import { useNavigation } from "@react-navigation/native";
+import { TextError } from "../../components/TextError";
 export default function SignInScreen() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   const navigation = useNavigation();
 
-  const handleSignIn = async () => {
+  const handleSignIn = async (data) => {
+    const { email, password } = data;
     const { error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
+      email,
+      password,
     });
 
     if (error) {
@@ -42,10 +56,9 @@ export default function SignInScreen() {
   return (
     <Container>
       <SpaceBetween width="85%">
-        
         <TitlesContainer>
           <Title size="22px">Seja bem vindo!</Title>
-          <SubTitle color={theme.colors.blueGray} size="14px"> 
+          <SubTitle color={theme.colors.blueGray} size="14px">
             Faça login para desbloquear todas as funções do aplicativo
           </SubTitle>
         </TitlesContainer>
@@ -57,18 +70,48 @@ export default function SignInScreen() {
           height="100%"
         />
       </SpaceBetween>
-
       <InputContainer>
-        <InputSmooth
-          placeholder="Email"
-          onChangeText={setEmail}
-          value={email}
+        <Controller
+          control={control}
+          name="email"
+          rules={{
+            required: "O email é obrigatório",
+            pattern: {
+              value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+              message: "Email inválido",
+            },
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <InputSmooth
+              placeholder="Email"
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+            />
+          )}
         />
-        <InputPasswordSmooth
-          placeholder="Senha"
-          onChangeText={setPassword}
-          value={password}
+        {errors.email && <TextError>{errors.email.message}</TextError>}
+
+        <Controller
+          control={control}
+          name="password"
+          rules={{
+            required: "A senha é obrigatória",
+            minLength: {
+              value: 6,
+              message: "A senha deve ter pelo menos 6 caracteres",
+            },
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <InputPasswordSmooth
+              placeholder="Senha"
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+            />
+          )}
         />
+        {errors.password && <TextError>{errors.password.message}</TextError>}
       </InputContainer>
 
       <LinkContainer>
@@ -77,7 +120,12 @@ export default function SignInScreen() {
         </LinkForgetPassword>
       </LinkContainer>
       <ButtonContainer>
-        <ButtonWithIcon text='Login' fontSize='18px' onPress={handleSignIn} color={theme.colors.black}/>
+        <ButtonWithIcon
+          text="Login"
+          fontSize="18px"
+          onPress={handleSubmit(handleSignIn)}
+          color={theme.colors.black}
+        />
       </ButtonContainer>
 
       <CreateAccountContainer>
